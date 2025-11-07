6d02f02ee07e1f0799dc74a9b4f2bae5b956eea9
Brian Lees <BrianLees@users.noreply.github.com>
2025-11-07 16:29:35 -0500

    Ability to convert a string to nodes

--- src/test_functions.py
+++ src/test_functions.py
@@ -225,5 +225,30 @@ class TestFunctions(unittest.TestCase):
             new_nodes,
         )
 
+    # text_to_textnodes
+    def test_split_everything(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        expected = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode("obi wan image", TextType.IMAGE,
+                     "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ]
+        actual = text_to_textnodes(text)
+        self.assertEqual(actual, expected)
+
+    def test_split_simple(self):
+        text = "This is simple"
+        expected = [TextNode("This is simple", TextType.TEXT),]
+        actual = text_to_textnodes(text)
+        self.assertEqual(actual, expected)
+
     if __name__ == "__main__":
         unittest.main()
